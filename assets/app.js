6b345e9f78a62b7663956a99d9195bd2151716f1
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-06-02 04:59:00 +0800

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -128,7 +128,7 @@ var links = [ //128
 ]
 
 var ads1 = [ //167
-    '<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',6008126,document.body||document.documentElement)</script>', 
+    'code', 
 ]
 
 var balance = 0;
